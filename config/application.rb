faa2593265b7ab35c8cf6043947091c86609f493
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-06-24 04:57:31 +0000

    タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+onfig.time_zone = 'Tokyo'
+config.active_record.default_timezone = :local
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
